98bfebf584367ea6233b44ddbad301b694e78c6b
Amirkhan5721 <amirkhanafridi959@gmail.com>
2024-10-09 09:12:00 -0700

    select element id through DOM

---
+++ script.js
@@ -0,0 +1,6 @@
+var weightInput = document.getElementById("weight");
+var heightInput = document.getElementById("height");
+var calculateBtn = document.getElementById("calculate-btn");
+var bmiResult = document.getElementById("bmi-result");
+var bmiCategory = document.getElementById("bmi-category");
+
