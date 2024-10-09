48e0db0b9e5f1fae1f29e772c28dd1c12ec6e9fe
Amirkhan5721 <amirkhanafridi959@gmail.com>
2024-10-09 09:27:13 -0700

    add event listener and create a function

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ var calculateBtn = document.getElementById("calculate-btn");
 var bmiResult = document.getElementById("bmi-result");
 var bmiCategory = document.getElementById("bmi-category");
 
+
+// Add event Listener to calculate button
+
+calculateBtn.addEventListener("click", bmiCalculator);
+
+function bmiCalculator (e) {
+    e.preventDefault();
+    var weight = parseFloat(weightInput.value);
+    var height = parseFloat(heightInput.value);
+    
+    if(isNaN(weight) || isNaN(height) || weight <= 0 || height <= 0) {
+        alert("Please Enter valid weight and height values");
+        return;
+    }
+
+    
+}
